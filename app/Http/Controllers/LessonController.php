63c48213e7820a7a18f97b5403aecac3a9de9f02
BaptisteO1 <baptiste.mr01@gmail.com>
2024-05-25 18:39:14 +0200

    (Coneo:0.1): Navigation leçons d\un cours

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -7,8 +7,16 @@ use Illuminate\Http\Request;
 
 class LessonController extends Controller
 {
+    // public function show(Lesson $lesson)
+    // {
+    //     return view('pages.lesson', ['lesson' => $lesson]);
+    // }
+
     public function show(Lesson $lesson)
     {
-        return view('pages.lesson', ['lesson' => $lesson]);
+        $nextLesson = $lesson->nextLesson();
+        $previousLesson = $lesson->previousLesson();
+        return view('pages.lesson', compact('lesson', 'nextLesson', 'previousLesson'));
     }
+
 }
--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Lesson extends Model
 {
@@ -12,5 +13,26 @@ class Lesson extends Model
     public function getRouteKeyName(): string
     {
         return 'slug';
+    }
+
+    public function nextLesson()
+    {
+        return self::where('course_id', $this->course_id)
+                    ->where('order', '>', $this->order)
+                    ->orderBy('order', 'asc')
+                    ->first();
+    }
+
+    public function previousLesson()
+    {
+        return self::where('course_id', $this->course_id)
+                    ->where('order', '<', $this->order)
+                    ->orderBy('order', 'desc')
+                    ->first();
+    }
+
+    public function course(): BelongsTo
+    {
+        return $this->belongsTo(Course::class);
     }
 }
